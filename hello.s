3673758e574d2aed2dfb123be567482fc54158be
Olivier Giroux <ogiroux@gmail.com>
2023-02-04 14:50:22 -0800

    Remove comment

--- hello.s
+++ hello.s
@@ -2,12 +2,12 @@
 .global _main
 .extern _puts
 _main: 
-        stp     x29, x30, [sp, #-16]!           // 16-byte Folded Spill
+        stp     x29, x30, [sp, #-16]!
         mov     x29, sp
         adr     x0, hello
         bl      _puts
         mov     w0, wzr
-        ldp     x29, x30, [sp], #16             // 16-byte Folded Reload
+        ldp     x29, x30, [sp], #16
         ret
 .align 4
 hello:
